3a83107c18e304bdab567f6e6c300a6b42537642
Andrew Michaelis <michaeaj@rose-hulman.edu>
2011-05-03 22:30:12 -0400

    Added ability to select global thermonuclear war

--- ChatBot-Attempt2/chatbot.rb
+++ ChatBot-Attempt2/chatbot.rb
@@ -2,9 +2,15 @@ STDOUT.sync = true
 puts 'What is your name?'
 name = gets.chomp
 puts 'Hello, #{name}!'
-puts 'Please select a game to play!'
+puts 'List of games available:'
 
 games = ["1. Global Thermonuclear War", "2. Apples and Oranges", "3. Chutes and Ladders", "4. Tag, You are it", "5. Life"]
 games.each do |i|
 puts i
+end
+puts 'Please select a game to play!'
+selection = gets.chomp
+
+if selection == '1'
+	puts 'Boom!'
 end
