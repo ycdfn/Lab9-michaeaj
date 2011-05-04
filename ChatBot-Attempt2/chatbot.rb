e45ea1cfa9fb22488daaa5e27725a92fa6163537
Andrew Michaelis <michaeaj@rose-hulman.edu>
2011-05-03 22:24:45 -0400

    Added content to the chatbot.rb file to print the list of games

--- ChatBot-Attempt2/chatbot.rb
+++ ChatBot-Attempt2/chatbot.rb
@@ -1,6 +1,10 @@
 STDOUT.sync = true
 puts 'What is your name?'
-
 name = gets.chomp
-
 puts 'Hello, #{name}!'
+puts 'Please select a game to play!'
+
+games = ["1. Global Thermonuclear War", "2. Apples and Oranges", "3. Chutes and Ladders", "4. Tag, You are it", "5. Life"]
+games.each do |i|
+puts i
+end
